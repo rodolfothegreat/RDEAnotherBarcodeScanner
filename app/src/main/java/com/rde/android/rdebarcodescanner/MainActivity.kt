bb2a2f1b3c337aa1630b767bf8e7ccca2c65f95d
Rodolfo d'Ettorre <rodolfothegreat@hotmail.com>
2021-05-02 23:16:47 +1000

    Initial commit

---
+++ app/src/main/java/com/rde/android/rdebarcodescanner/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.rde.android.rdebarcodescanner
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
